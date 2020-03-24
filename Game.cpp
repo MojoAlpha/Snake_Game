db228597b67d85e4040ddf8bd7ad4f218bfc1b5d
MojoAlpha <mohitp@mnnit.ac.in>
2020-03-24 11:30:27 +0530

    Movement & Score

--- Game.cpp
+++ Game.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<conio.h>
-#include<dos.h>
 #include<cstdlib>
 using namespace std;
 class SnakeHd
@@ -10,10 +9,13 @@ class SnakeHd
   char dir;
 }S;
 const int Height=20;
-const int width=40;
+const int width=20;
+int tailX[1000],tailY[1000];
 bool GameEnd;
 int foodX,foodY;
+long Points;
 void Setup();
+void InpLog();
 void Screen();
 int main()
 {
@@ -21,7 +23,10 @@ int main()
   while(!GameEnd)
   {
      Screen();
+     InpLog();
   }
+  printf("\nGAME OVER!!");
+  getch();
   return 0;
 }
 void Setup()
@@ -31,6 +36,7 @@ S.x=Height/2;
 S.y=width/2;
 foodX=rand()%width;
 foodY=rand()%Height;
+Points=0;
 }
 void Screen()
 {
@@ -42,14 +48,44 @@ void Screen()
   {
     for(int j=0;j<width;++j)
     {
-      if (j==0||j==width-1)
+      if (j==0)
         printf("#");
-
-        printf(" ");
+      if(i==foodY && j==foodX)
+          {printf("@");
+          continue;}
+      if(i==S.y && j==S.x)
+        {printf("O");
+        continue;}
+      if(j!=0&&j!=width)
+          printf(" ");
+      if(j==width-1)
+        printf("#");
     }
     printf("\n");
   }
 
   for(int i=0;i<width+1;++i)  //displaying horizontal walls
      printf("#");
+  printf("\n");
+  printf("Points : %ld",Points);
+}
+void InpLog()
+{
+  if(kbhit())
+  S.dir=getch();
+    switch (S.dir) {
+      case 'a': S.x--; break;
+      case 'd': S.x++; break;
+      case 'w': S.y--; break;
+      case 's': S.y++; break;
+      case 'x': GameEnd=true;
+    }
+  if(S.x==foodX && S.y==foodY)
+  {
+    Points+=10;
+    foodX=rand()%width;
+    foodY=rand()%Height;
+  }
+  if(S.x==-1 || S.x==width || S.y==-1 || S.y==Height)
+  GameEnd=true;
 }
